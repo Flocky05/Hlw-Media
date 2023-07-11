54f42d38a37aa7bd0b14539de480a7d781065df6
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-07-12 01:38:31 +0600

    added post section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Post from "./component/Post/Post";
 import Footer from "./component/Shared/Footer/Footer";
 import Navbar from "./component/Shared/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Post></Post>
       <Footer></Footer>
     </div>
   );
---
+++ src/component/Post/Post.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <div className="carousel rounded-box">
+      <div className="carousel-item">
+        <img
+          src="https://images.unsplash.com/photo-1493670125474-bf86b93281b0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1311&q=80"
+          alt="Burger"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="https://images.unsplash.com/photo-1500043357865-c6b8827edf10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          alt="Burger"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="https://images.unsplash.com/photo-1501264313730-613339950144?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          alt="Burger"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="https://images.unsplash.com/photo-1446034295857-c39f8844fad4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          alt="Burger"
+        />
+      </div>
+      <div className="carousel-item">
+        <img
+          src="https://images.unsplash.com/photo-1476610182048-b716b8518aae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1259&q=80"
+          alt="Burger"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Post;
